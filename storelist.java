b8c802f0053f5185d1f4aab0085d822dab217634
riskypvtmiller <luke91695@gmail.com>
2024-07-30 12:36:56 -0400

    Update storelist.java

--- storelist.java
+++ storelist.java
@@ -1,1 +1,35 @@
 
+import java.util.ArrayList;
+//import java.util.List;
+
+public class Storelist {
+
+    public static void main(String[] args) {
+        ArrayList<String> Store = new ArrayList<>();
+        Object randomgen = Store;
+
+        Store.add("Blacksmith");
+        Store.add("Armory");
+        Store.add("Adventuring Supplies");
+        Store.add("General Store");
+        Store.add("Jeweler");
+        Store.add("Stonecutter");
+        Store.add("Leatherworker");
+        Store.add("Woodworker");
+        Store.add("Bowyer");
+        Store.add("Shady Dealer");
+        Store.add("Tailor");
+        Store.add("Textiles");
+        Store.add("Potion Shop");
+        Store.add("Temple of Faith");
+        Store.add("Arcane Shop");
+        Store.add("Inn");
+        Store.add("Tavern");
+        Store.add("Stable");
+        Store.add("Farm Animals");
+        Store.add("Dock");
+        Store.add("Shipyard");
+        System.out.println(randomgen);
+    }
+}
+
